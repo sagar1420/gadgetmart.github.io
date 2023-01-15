56982ce9b74c4237ef0e46f67bcb01383f30bcf2
sagar <sagar1420.be20@chitkara.edu.in>
2023-01-14 23:35:21 -0700

    gadget mart

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 40000,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro",
+    price: 60000,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon N32 Camera",
+    price: 10000,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Jacket",
+    price: 500,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Bulb 40 W",
+    price: 60,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "T-shirt",
+    price: 120,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Video Game",
+    price: 12000,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "HAT",
+    price: 40,
+    productImage: product8,
+  },
+];
